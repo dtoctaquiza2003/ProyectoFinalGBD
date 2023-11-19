918d1f65ae0c5e572d9afb6e69d15c242d882f88
MiltonUrgiles <Jolu20x@gmail.com>
2023-11-19 16:53:34 -0500

    actualizacion webApi

--- app.py
+++ app.py
@@ -12,6 +12,79 @@ db = None  # Inicialmente, la base de datos MongoDB está configurada como None
 def formulario():
     return render_template('formulario.html')
 
+def obtener_tablas(servidor, usuario, contraseña, nombre_base):
+    # Función para obtener las tablas de una base de datos específica
+
+    # Conexión a MySQL especificando el servidor, usuario, contraseña y base de datos
+    conn = mysql.connector.connect(
+        host=servidor,
+        user=usuario,
+        password=contraseña,
+        database=nombre_base
+    )
+
+    # Crear un cursor para ejecutar consultas SQL
+    cursor = conn.cursor()
+
+    # Obtener las tablas de la base de datos actual
+    cursor.execute("SHOW TABLES")
+    tablas = cursor.fetchall()
+
+    # Convertir el resultado a una lista de nombres de tablas
+    lista_de_tablas = [tabla[0] for tabla in tablas]
+
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    conn.close()
+
+    return lista_de_tablas
+
+@app.route('/conectar_mysql', methods=['POST'])
+def conectar_mysql():
+    if request.method == 'POST':
+        servidor = request.form['mysql_server']
+        usuario = request.form['mysql_username']
+        contraseña = request.form['mysql_password']
+
+        try:
+            # Conexión a MySQL sin especificar la base de datos
+            conn = mysql.connector.connect(
+                host=servidor,
+                user=usuario,
+                password=contraseña
+            )
+
+            # Crear un cursor para ejecutar consultas SQL
+            cursor = conn.cursor()
+
+            # Obtener todas las bases de datos
+            cursor.execute("SHOW DATABASES")
+            bases_de_datos = cursor.fetchall()
+
+            # Inicializar un diccionario para almacenar las tablas de cada base de datos
+            diccionario_tablas = {}
+
+            for db in bases_de_datos:
+                nombre_base = db[0]
+
+                # Filtrar bases de datos del sistema
+                if not nombre_base.startswith(('mysql', 'information_schema', 'performance_schema')):
+                    # Obtener las tablas utilizando la función auxiliar
+                    lista_de_tablas = obtener_tablas(servidor, usuario, contraseña, nombre_base)
+
+                    # Almacenar las tablas en el diccionario
+                    diccionario_tablas[nombre_base] = lista_de_tablas
+
+            # Cerrar el cursor y la conexión
+            cursor.close()
+            conn.close()
+
+            # Renderizar el template con la información obtenida
+            return render_template('mysql.html', bases_y_tablas=diccionario_tablas)
+        except mysql.connector.Error as err:
+            return jsonify({'error': f"Error de conexión a MySQL: {err}"}), 500
+
+
 @app.route('/conectar_mongodb', methods=['POST','GET'])
 def conectar_mongodb():
     #Configura la conexión a la base de datos MongoDB
